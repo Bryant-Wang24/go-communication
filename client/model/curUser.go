fdb3f5ba0db80e9276d0f7c4396a2fe3d47f146f
Bryant-Wang24 <485969746@qq.com>
2022-09-23 15:32:31 +0800

    :bug: 包之间避免循环引用

--- client/model/curUser.go
+++ client/model/curUser.go
@@ -1,11 +1,10 @@
 package model
 
 import (
-	"client/message"
 	"net"
 )
 
 type CurUser struct {
 	Conn net.Conn
-	message.User
+	User
 }
--- client/model/user.go
+++ client/model/user.go
@@ -2,7 +2,9 @@ package model
 
 // User 定义一个用户的结构体
 type User struct {
-	UserId   int    `json:"userId"`
-	UserPwd  string `json:"userPwd"`
-	UserName string `json:"userName"`
+	UserId     int    `json:"userId"`
+	UserPwd    string `json:"userPwd"`
+	UserName   string `json:"userName"`
+	UserStatus int    `json:"userStatus"`
+	Sex        string `json:"sex"`
 }
